7fa20ab74f2d0a76f52f5bcb0a765f8d4c6034f1
itachiuchiha084 <130834968+itachiuchiha084@users.noreply.github.com>
2023-05-30 11:14:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,17 +6,17 @@ if os.path.exists("config.env"):
 elif os.path.exists("sample.env"):
     load_dotenv("sample.env")
 
-API_ID = os.getenv("API_ID","16743442")
-API_HASH = os.getenv("API_HASH","12bbd720f4097ba7713c5e40a11dfd2a")
-BOT_TOKEN = os.getenv("BOT_TOKEN","6065321925:AAHOUFD7idBBq-Rnud0hRyitSH_f_S43vqk")
-MONGO_DB_URI = os.getenv("MONGO_DB_URI","mongodb+srv://sonu55:sonu55@cluster0.vqztrvk.mongodb.net/?retryWrites=true&w=majority")
-STATUS_MSG_ID = os.getenv("STATUS_MSG_ID","Index_AC/2")
-SCHEDULE_MSG_ID = os.getenv("SCHEDULE_MSG_ID","Upload_AC/2")
-CHANNEL_TITLE = os.getenv("CHANNEL_TITLE", "Anime_Campus")
-INDEX_CHANNEL_USERNAME = os.getenv("INDEX_CHANNEL_USERNAME","Index_AC")
-UPLOADS_CHANNEL_USERNAME = os.getenv("UPLOADS_CHANNEL_USERNAME","Upload_AC")
-TECHZ_API_KEY = os.getenv("TECHZ_API_KEY","NWORVK")
-COMMENTS_GROUP_LINK = os.getenv("COMMENTS_GROUP_LINK", "https://t.me/+WwBuJV85gB5lZTNl")
+API_ID = os.getenv("API_ID","27108998")
+API_HASH = os.getenv("API_HASH","f4fc03493766db361d7e85ed8974fe2f")
+BOT_TOKEN = os.getenv("BOT_TOKEN","5989024511:AAGQuNKayfE0n--SV5o4ONgyWdQKVpQDZaI")
+MONGO_DB_URI = os.getenv("MONGO_DB_URI","mongodb+srv://naruto:naruto@cluster0.h9wsftd.mongodb.net/?retryWrites=true&w=majority")
+STATUS_MSG_ID = os.getenv("STATUS_MSG_ID","Anime_Alliance/3")
+SCHEDULE_MSG_ID = os.getenv("SCHEDULE_MSG_ID","anime_ongoing_uploadsC/2")
+CHANNEL_TITLE = os.getenv("CHANNEL_TITLE", "Anime_Alliance")
+INDEX_CHANNEL_USERNAME = os.getenv("INDEX_CHANNEL_USERNAME","Anime_Alliance")
+UPLOADS_CHANNEL_USERNAME = os.getenv("UPLOADS_CHANNEL_USERNAME","anime_ongoing_uploads")
+TECHZ_API_KEY = os.getenv("TECHZ_API_KEY","EQRQFH")
+COMMENTS_GROUP_LINK = os.getenv("COMMENTS_GROUP_LINK", "https://t.me/+5-_jTUAhGTpiY2Vl")
 SLEEP_TIME = os.getenv("SLEEP_TIME", 60)
 
 for k, v in {
